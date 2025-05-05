3955db2f1ab13494b30c26070759fc0feb169dd0
victor <vshulpov@gmail.com>
2025-05-05 23:52:31 +0700

    get spots with access token

--- xmap-auth2-pkce-probe/src/app/data/data.component.ts
+++ xmap-auth2-pkce-probe/src/app/data/data.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-data',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf
+  ],
   templateUrl: './data.component.html',
   styleUrl: './data.component.css'
 })
 export class DataComponent {
+  spots: Array<any> = [];
 
+  constructor(private http: HttpClient) {
+  }
+
+  public getSpots() {
+    this.http.get("https://localhost:8090/api/v1/spot/all", {
+        headers: {
+          "Authorization": "Bearer " + localStorage.getItem("access_token"),
+          "Content-Type": "application/json"
+        }
+      }
+    ).subscribe(
+      {next: (response: any) => {
+          console.log(response);
+          this.spots = response.content;
+        },
+        error: error => {
+          console.log(error);}
+      }
+    );
+  }
 }
